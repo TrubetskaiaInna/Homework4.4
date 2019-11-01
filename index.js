26f3f59113285ce416cdd0fb05bcd7de4b632d20
Trubetskaia Inna <trubetskaya_inna@ukr.net>
2019-11-01 20:06:13 +0200

    created function that walks along all elements

--- index.js
+++ index.js
@@ -4,13 +4,34 @@ const passSnail = (cols, rows, c0, r0) => {
 
   arrResult.push([r0, c0])
 
-  c0 = c0 + 1
-  if (r0 >= 0 && r0 < rows && c0 >= 0 && c0 < cols) {
-    arrResult.push([r0, c0])
+  for (let i = 1; i < numberIter + 3; i++) {
+    if (i % 2 === 0) {
+      for (let j = 1; j < i + 1; j++) {
+        c0 = c0 - 1
+        if (r0 >= 0 && r0 < rows && c0 >= 0 && c0 < cols) {
+          arrResult.push([r0, c0])
+        }
+      }
+      for (let j = 1; j < i + 1; j++) {
+        r0 = r0 - 1
+        if (r0 >= 0 && r0 < rows && c0 >= 0 && c0 < cols) {
+          arrResult.push([r0, c0])
+        }
+      }
+    } else {
+      for (let j = 1; j < i + 1; j++) {
+        c0 = c0 + 1
+        if (r0 >= 0 && r0 < rows && c0 >= 0 && c0 < cols) {
+          arrResult.push([r0, c0])
+        }
+      }
+      for (let j = 1; j < i + 1; j++) {
+        r0 = r0 + 1
+        if (r0 >= 0 && r0 < rows && c0 >= 0 && c0 < cols) {
+          arrResult.push([r0, c0])
+        }
+      }
+    }
   }
-
-  r0 = r0 + 1
-  if (r0 >= 0 && r0 < rows && c0 >= 0 && c0 < cols) {
-    arrResult.push([r0, c0])
-  }
+  return arrResult
 }
